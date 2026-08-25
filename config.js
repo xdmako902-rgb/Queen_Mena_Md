00f574bece5b34257934bd258857f5379a6de515
@Mako'Z <xdmako902@gmail.com>
2026-08-25 08:01:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,49 @@
+module.exports = {
+    PREFIX: ".",
+    OWNER_NUMBER: "94766398472",
+    BOT_NAME: "𝐐𝐔𝐄𝐄𝐍 𝐌𝐄𝐍𝐀 𝐌𝐃",
+    WORK_TYPE: "public",
+
+    AUTO_TYPING: false,
+    AUTO_RECORDING: true,
+    ALWAYS_ONLINE: false,
+    ALWAYS_OFFLINE: true,
+    AUTO_READ: false,
+    READ_CMD_ONLY: true,
+    AUTO_BIO: true,
+
+    AUTO_REACT: true,
+    REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
+
+    AUTO_READ_STATUS: true,
+    AUTO_VIEW_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
+
+    ANTI_BOT: false,
+    ANTI_BAD: false,
+    ANTI_LINK: false,
+    ANTI_DELETE: "true",
+    DELETEMSGSENDTO: "owner",
+        
+    // ප්‍රධාන චැනල් එක
+    NEWSLETTER_JID: "120363428181646517@newsletter",
+
+    // ඔටෝ ෆලෝව් විය යුතු චැනල් 10 ක JID ලැයිස්තුව (මෙතැනට ඔබේ චැනල් 10 දාන්න)
+    NEWSLETTER_JIDS: [
+        "120363412261622272@newsletter", // චැනල් 01
+        "YOUR_CHANNEL_JID_2@newsletter",  // චැනල් 02
+        "YOUR_CHANNEL_JID_3@newsletter",  // චැනල් 03
+        "YOUR_CHANNEL_JID_4@newsletter",  // චැනල් 04
+        "YOUR_CHANNEL_JID_5@newsletter",  // චැනල් 05
+        "YOUR_CHANNEL_JID_6@newsletter",  // චැනල් 06
+        "YOUR_CHANNEL_JID_7@newsletter",  // චැනල් 07
+        "YOUR_CHANNEL_JID_8@newsletter",  // චැනල් 08
+        "YOUR_CHANNEL_JID_9@newsletter",  // චැනල් 09
+        "YOUR_CHANNEL_JID_10@newsletter"  // චැනල් 10
+    ],
+
+    GROUP_INVITE_LINK: "https://chat.whatsapp.com/L3uA3FqcKxU3B2FlMd52Sh",
+    MAX_RETRIES: 3,
+    RCD_IMAGE_PATH: "https://i.ibb.co/bMWcfQ3j/66170b62f2de.jpg"
+};
